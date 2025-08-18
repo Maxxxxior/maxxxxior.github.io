5581c1825903ebb79087b6a745882a686dd40668
MaxxxxiorS <Maksymilian.Podlecki@gmail.com>
2025-08-18 07:07:20 +0200

    ✨Some changes
    Added:
    - my repositories
      - auto sort for them
    - avatar and my nickname with link to github
    Changed:
    - name of button for theme change from `Clair / Obscur` to `Change theme`

--- javascript.js
+++ javascript.js
@@ -1,13 +1,17 @@
 // I18N
 const I18N = {
     pl: {
+        changeTheme: 'Zmień motyw',
         myProjects: 'Moje projekty',
+        myRepos: 'Moje repozytoria',
         internProject: 'Projekt na praktyki',
         websiteWChUW: 'Strona dla Wydziału Chemii Uniwersytetu Warszawskiego (WChUW)',
         createdBy: 'Stworzone przez'
     },
     en: {
+        changeTheme: 'Change theme',
         myProjects: 'My projects',
+        myRepos: 'My github repositories',
         internProject: 'Project for my intern',
         websiteWChUW: 'Website for the Faculty of Chemistry, University of Warsaw (WChUW)',
         createdBy: 'Created by'
@@ -52,4 +56,18 @@ function setLang(lang) {
         const key = el.getAttribute('data-i18n');
         el.textContent = I18N[lang][key];
     });
+}
+
+// Sort repos alphabetical order
+function sortRepos() {
+    const container = document.querySelector(".repos");
+    const allRepos = Array.from(container.querySelectorAll("a.repo"));
+
+    const normalRepos = allRepos.filter(a => a.hasAttribute("href"));
+    const specialRepos = allRepos.filter(a => !a.hasAttribute("href"));
+
+    normalRepos.sort((a, b) => a.textContent.toLowerCase().localeCompare(b.textContent.toLowerCase()));
+
+    normalRepos.forEach(a => container.appendChild(a));
+    specialRepos.forEach(a => container.appendChild(a));
 }
